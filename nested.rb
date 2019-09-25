23dda02a9dce0e8d2de9faeaaef39575de91386f
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-09-25 23:50:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,9 +83,9 @@ def adding_matz
      }
 
     programmer_hash[:yukihiro_matsumoto] = {}
-		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-		 programmer_hash
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+    programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+    programmer_hash
 end
 
 def changing_alan
@@ -105,10 +105,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
      programmer_hash[:alan_kay][:known_for] = alans_new_info
-	programmer_hash
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -127,7 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-	programmer_hash
+     
     
 end
